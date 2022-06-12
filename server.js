2f19a966cd4c6f9e86ad143a9e4d3bb0a5ac0f83
C22-PS171 <106735115+C22-PS171@users.noreply.github.com>
2022-06-13 01:10:56 +0700

    Update server.js

--- server.js
+++ server.js
@@ -120,27 +120,19 @@ app.post("/refield/login", function (req, res) {
 });
 
 //post account
-app.post("/refield/signup",(req, res) => {
-  let data = {email: req.body.email, password: req.body.password};
-  let sql = "INSERT INTO account SET ?";
-  let query = conn.query(sql, data,(err, results) => {
-    if(err) throw err;
-    res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
-  });
+app.post("/refield/signup", (req, res) => {
+  try {
+    let sql = `INSERT INTO account(email, password) VALUES (?)`;
+
+    let values = [req.body.email, md5(req.body.password)];
+
+    conn.query(sql, [values], (err, results) => {
+      res.send(JSON.stringify({ error: err, response: results }));
+    });
+  } catch (error) {
+    return error.message;
+  }
 });
-// app.post("/refield/signup", function (req, res) {
-//   try {
-//     let sql = `INSERT INTO account(email, password) VALUES (?)`;
-
-//     let values = [req.body.email, md5(req.body.password)];
-
-//     conn.query(sql, [values], (err, results) => {
-//       res.send(JSON.stringify({ error: err, response: results }));
-//     });
-//   } catch (error) {
-//     return error.message;
-//   }
-// });
 
 //Server listening
 var port = process.env.PORT || 5000;
